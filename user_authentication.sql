0234349cc2e456e85531768c566ed598d18a8e68
jerome bernante <jeromebernante@gmail.com>
2024-09-25 04:08:43 +0800

    update

--- user_authentication.sql
+++ user_authentication.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 24, 2024 at 01:50 PM
+-- Generation Time: Sep 24, 2024 at 10:08 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,58 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `contact_messages`
+--
+
+CREATE TABLE `contact_messages` (
+  `contact_message_id` int(11) NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `contact_messages`
+--
+
+INSERT INTO `contact_messages` (`contact_message_id`, `full_name`, `email`, `subject`, `message`, `created_at`) VALUES
+(1, 'Jerome Bernante', 'jeromebernante@gmail.com', 'issue', 'I don\'t know yet', '2024-09-25 01:45:21'),
+(2, 'Jerome Bernante', 'jeromebernante@gmail.com', 'issue2', 'yes test 2', '2024-09-25 01:45:22'),
+(3, 'Jerome Bernante', 'jeromebernante@gmail.com', 'issue4', 'test4', '2024-09-25 01:45:23'),
+(4, 'Jerome Bernante', 'jeromebernante@gmail.com', 'issue5', 'test5', '2024-09-25 01:47:21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_attempts`
+--
+
+CREATE TABLE `login_attempts` (
+  `login_attempt_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `timestamp` datetime DEFAULT NULL,
+  `success` tinyint(1) NOT NULL,
+  `user_agent` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `login_attempts`
+--
+
+INSERT INTO `login_attempts` (`login_attempt_id`, `user_id`, `username`, `ip_address`, `timestamp`, `success`, `user_agent`) VALUES
+(1, 1, 'adminjerome', '::1', '2024-09-25 02:49:13', 0, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'),
+(2, 1, 'adminjerome', '::1', '2024-09-25 02:49:36', 1, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'),
+(3, 1, 'adminjerome', '::1', '2024-09-25 03:01:02', 1, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'),
+(4, 1, 'adminjerome', '::1', '2024-09-25 03:35:26', 1, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'),
+(5, 1, 'adminjerome', '::1', '2024-09-25 03:39:33', 1, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -35,8 +87,27 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `full_name`) VALUES
+(1, 'adminjerome', '$2y$10$ES48ki0a0Iw5oG2Pm52bLux2UE8ycI4u1MstxdCoCCI1LpSv.uBHW', 'jerome bernante');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `contact_messages`
+--
+ALTER TABLE `contact_messages`
+  ADD PRIMARY KEY (`contact_message_id`);
+
+--
+-- Indexes for table `login_attempts`
+--
+ALTER TABLE `login_attempts`
+  ADD PRIMARY KEY (`login_attempt_id`);
 
 --
 -- Indexes for table `users`
@@ -49,10 +120,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `contact_messages`
+--
+ALTER TABLE `contact_messages`
+  MODIFY `contact_message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `login_attempts`
+--
+ALTER TABLE `login_attempts`
+  MODIFY `login_attempt_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
